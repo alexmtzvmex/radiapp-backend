da68e7d6120fec802f6542a26546a450c045b2a3
altaradio <hector.martinez15cfe@gmail.com>
2026-05-16 18:50:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const http = require("http");
-const channelRoutes = require("./routes/channels.routes");
 const socketIO = require("socket.io");
 const cors = require("cors");
 require("dotenv").config();
@@ -8,6 +7,7 @@ require("dotenv").config();
 const db = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
 const adminRoutes = require("./routes/admin.routes");
+const channelRoutes = require("./routes/channels.routes");
 
 const app = express();
 const server = http.createServer(app);
@@ -42,12 +42,47 @@ app.get("/", async (req, res) => {
         app: "RadiApp Backend",
         status: "ONLINE",
         mysql: mysqlStatus,
+        socket: "READY",
         version: "1.0.0"
     });
 });
 
 io.on("connection", (socket) => {
     console.log("Usuario conectado:", socket.id);
+
+    socket.on("entrar_canal", (data) => {
+        const canalId = data.canal_id;
+        const usuario = data.usuario || "Usuario";
+
+        socket.join("canal_" + canalId);
+
+        io.to("canal_" + canalId).emit("mensaje_canal", {
+            tipo: "sistema",
+            mensaje: usuario + " entró al canal " + canalId
+        });
+    });
+
+    socket.on("ptt_inicio", (data) => {
+        const canalId = data.canal_id;
+        const usuario = data.usuario || "Usuario";
+
+        io.to("canal_" + canalId).emit("ptt_estado", {
+            hablando: true,
+            usuario: usuario,
+            mensaje: usuario + " está hablando..."
+        });
+    });
+
+    socket.on("ptt_fin", (data) => {
+        const canalId = data.canal_id;
+        const usuario = data.usuario || "Usuario";
+
+        io.to("canal_" + canalId).emit("ptt_estado", {
+            hablando: false,
+            usuario: usuario,
+            mensaje: usuario + " dejó de hablar."
+        });
+    });
 
     socket.on("disconnect", () => {
         console.log("Usuario desconectado:", socket.id);
